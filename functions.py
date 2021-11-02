79d95c1e3b415d0a8b9f131dc4a8df56c5f6ed9c
wHo69 <hu_bowen@students.edu.sg>
2021-11-02 18:06:26 +0800

    Added file read write functions

--- functions.py
+++ functions.py
@@ -2,6 +2,10 @@
 # A place to store all the "important" functions for the bot lol
 # Author: wHo#6933
 
+# Libraries
+import json
+
+# Admin matters
 async def get_admins():
     # Returns a list of admin IDs, stored in STRINGs
     with open("admins.txt")as f:
@@ -15,3 +19,16 @@ async def check_admin(id: str):
     admins = await get_admins()
     return (id in admins) # if id in admins, yes
                           # if id not in admins, no
+                        
+# Suggestion file handling
+async def readSuggestions():
+    with open("./Suggestions/suggestions.json", "r")as f:
+        data = json.load(f)
+        f.close()
+
+    return data
+
+async def writeSuggestions(dic : dict):
+    with open("./Suggestions/suggestions.json", "w")as f:
+        data = json.dump(dic, f, indent=4) # indent for formatting
+        f.close()
